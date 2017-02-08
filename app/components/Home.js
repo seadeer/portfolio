a63007ba802a8b4bbae7d435c23654d33f01c960
Anna Clawson <seadeer@gmail.com>
2017-02-07 16:29:58 -0800

    mouse event handling for animation

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,19 +8,24 @@ var Home = React.createClass({
     getInitialState: function(){
         return{
             mouseX: 0,
-            mouseY: 0
+            mouseY: 0,
+            lastMove: 0
         };
     },
     HandleMouse: function(event){
-        event.preventDefault();
-        this.setState({
+//        event.preventDefault();
+        if(Date.now() - this.state.lastMove > 50){
+            this.setState({
             mouseX: event.clientX,
-            mouseY: event.clientY
+            mouseY: event.clientY,
+            lastMove: Date.now()
         })
+        }
     },
     render: function(){
        return(
        <div onMouseMove={(event) => this.HandleMouse(event)} className="container" >
+            <a href name="top"></a>
             <Circle x={this.state.mouseX} y={this.state.mouseY}/>
             <About/>
             <Projects/>
